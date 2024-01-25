d76e3089710035944c2f0c279c23012813d900ea
Aby George <aby.george@iqvia.com>
2024-01-26 02:12:00 +0530

    Building it back up

--- ActivityManager/IDE.py
+++ ActivityManager/IDE.py
@@ -13,35 +13,35 @@ def choose_folder(entry_widget):
     if folder_selected:
         entry_widget.delete(0, tk.END)  # Clear the current entry
         entry_widget.insert(0, folder_selected)  # Insert the selected folder path
-        available_flows = pull_activities(folder_selected)  # Call pull_activities with the folder path
+        available_activities = pull_activities(folder_selected)  # Call pull_activities with the folder path
         bordered_edit_text = '▌Edit▐' # Unicode characters for left and right border
 
-        for index, activity in enumerate(available_flows):
+        for index, activity in enumerate(available_activities):
             tag = 'oddrow' if index % 2 else ''  # Apply tag for alternating row colors
             #activity_path = f"{activity[0]}  -  {activity[1]}"
             print(activity[0])
-            available_flows_tree.insert('', tk.END, values=(activity[0], bordered_edit_text), tags=(tag,))
+            available_activities_tree.insert('', tk.END, values=(activity[0], bordered_edit_text), tags=(tag,))
 
         # Call the stripe function for the available activities tree
-        stripe_rows(available_flows_tree)
-        stripe_rows(chosen_flows_tree)
-        style_edit_column(available_flows_tree)
+        stripe_rows(available_activities_tree)
+        stripe_rows(chosen_activities_tree)
+        style_edit_column(available_activities_tree)
 
 
 def show_tooltip(event, tree):
     row_id = tree.identify_row(event.y)
     if row_id:
         item_text = tree.item(row_id, 'values')[0]
-        create_tooltip(available_flows_tree)
-        create_tooltip(chosen_flows_tree)
+        create_tooltip(available_activities_tree)
+        create_tooltip(chosen_activities_tree)
 
 
-def create_flow():
+def create_activity():
     # Implement the function to create an activity
     pass
 
-def add_flow():
-    # Implement the function to add a flow
+def add_activity():
+    # Implement the function to add a activity
     pass
 
 def choose_file(entry):
@@ -50,7 +50,7 @@ def choose_file(entry):
     entry.insert(0, filename)
 
 
-def delete_from_chosen_flows(event):
+def delete_from_chosen_activities(event):
     tree = event.widget
     item_id = tree.focus()
     if item_id:
@@ -64,13 +64,13 @@ def on_treeview_click(event):
         edit_activity(activity_name, event.widget)
 
 
-def update_chosen_flows():
-    for i, item in enumerate(chosen_flows_tree.get_children()):
+def update_chosen_activities():
+    for i, item in enumerate(chosen_activities_tree.get_children()):
         tag = 'oddrow' if i % 2 else ''
         print(f"The item is : {item}")
-        chosen_flows_tree.item(item, tags=(tag,))
+        chosen_activities_tree.item(item, tags=(tag,))
 
-def add_to_chosen_flows(event):
+def add_to_chosen_activities(event):
     tree = event.widget
     item_id = tree.focus()
     if item_id:
@@ -78,8 +78,8 @@ def add_to_chosen_flows(event):
         activity_name = item_values[0]
         full_text = f"{activity_name}"
         print(f"Chosen item id : {item_id} and activity name : {full_text}")
-        chosen_flows_tree.insert('', tk.END, values=(full_text,), tags=('oddrow',))
-        update_chosen_flows()
+        chosen_activities_tree.insert('', tk.END, values=(full_text,), tags=('oddrow',))
+        update_chosen_activities()
 
 def style_edit_column(tree):
     tree.tag_configure('editTag', background='lightblue', font=('Arial', 10, 'bold'))  # Set background color and bold font
@@ -103,45 +103,45 @@ def edit_activity(activity_name, tree):
 
 def generate_excel():
     base_excel_path='C:\\Users\\u1138322\\PAF\\ProjectContainer\\SampleProject\\excel'
-    selected_items = chosen_flows_tree.selection()  # Get selected items in the treeview
+    selected_items = chosen_activities_tree.selection()  # Get selected items in the treeview
     if selected_items:
-        selected_flow = chosen_flows_tree.item(selected_items[0], 'values')[0]
-        print(f"Generating excel for: {selected_flow}")
-        parts = selected_flow.split('   PATH : ')
-        flow_name = parts[0]
+        selected_activity = chosen_activities_tree.item(selected_items[0], 'values')[0]
+        print(f"Generating excel for: {selected_activity}")
+        parts = selected_activity.split('   PATH : ')
+        activity_name = parts[0]
         path = parts[1]        
-        base_excel_path = create_excel(flow_name, base_excel_path)
-        activity_list = retrieve_activities(flow_name, path)
+        base_excel_path = create_excel(activity_name, base_excel_path)
+        activity_list = retrieve_activities(activity_name, path)
         updated_activity_list = update_activity_paths(activity_list, base_excel_path)
         insert_recorder_id(updated_activity_list)
         extract_data_and_write_to_excel(updated_activity_list, base_excel_path)
         
 
     else:
-        # If no flow is selected, show an alert
-        messagebox.showinfo("No Selection", "Please select a flow you want to generate the excel for")
+        # If no activity is selected, show an alert
+        messagebox.showinfo("No Selection", "Please select a activity you want to generate the excel for")
 
 def duplicate_excel():
     base_excel_path='C:\\Users\\u1138322\\PAF\\ProjectContainer\\SampleProject\\excel'
-    selected_items = chosen_flows_tree.selection()  # Get selected items in the treeview
+    selected_items = chosen_activities_tree.selection()  # Get selected items in the treeview
     if selected_items:
-        selected_flow = chosen_flows_tree.item(selected_items[0], 'values')[0]
-        print(f"Generating excel for: {selected_flow}")
-        parts = selected_flow.split('   PATH : ')
-        flow_name = parts[0]
+        selected_activity = chosen_activities_tree.item(selected_items[0], 'values')[0]
+        print(f"Generating excel for: {selected_activity}")
+        parts = selected_activity.split('   PATH : ')
+        activity_name = parts[0]
         path = parts[1]
-        file_name = f"{flow_name}.xlsx"
+        file_name = f"{activity_name}.xlsx"
         file_path = os.path.join(base_excel_path, file_name)
         if not os.path.isfile(file_path):
-            messagebox.showinfo("File Not Found", "There is no excel for this flow. Please generate an excel before you duplicate it.")
+            messagebox.showinfo("File Not Found", "There is no excel for this activity. Please generate an excel before you duplicate it.")
         else:
-            open_duplicate_window(file_path, chosen_flows_tree)
+            open_duplicate_window(file_path, chosen_activities_tree)
     
     else:
-        # If no flow is selected, show an alert
-        messagebox.showinfo("No Selection", "Please select a flow you want to generate the excel for")
+        # If no activity is selected, show an alert
+        messagebox.showinfo("No Selection", "Please select a activity you want to generate the excel for")
 
-def open_duplicate_window(base_excel_path, chosen_flows_tree):
+def open_duplicate_window(base_excel_path, chosen_activities_tree):
     # New window for duplicating the file
     duplicate_window = tk.Toplevel()
     duplicate_window.title("Duplicate Excel File")
@@ -156,7 +156,7 @@ def open_duplicate_window(base_excel_path, chosen_flows_tree):
     num_duplicates_entry = ttk.Entry(content_frame, font=("Arial", 10))
     num_duplicates_entry.pack(side=tk.TOP, fill=tk.X, pady=(0, 10))
     # Duplicate button
-    duplicate_button = ttk.Button(content_frame, text="Duplicate", command=lambda: duplicate_file(duplicate_window, num_duplicates_entry, base_excel_path, chosen_flows_tree))
+    duplicate_button = ttk.Button(content_frame, text="Duplicate", command=lambda: duplicate_file(duplicate_window, num_duplicates_entry, base_excel_path, chosen_activities_tree))
     duplicate_button.pack(side=tk.TOP, pady=(0, 10))
 
     # Center the window on the screen
@@ -171,7 +171,7 @@ def center_window_on_screen(window):
     window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 
-def duplicate_file(duplicate_window, num_duplicates_entry, base_excel_path, chosen_flows_tree):
+def duplicate_file(duplicate_window, num_duplicates_entry, base_excel_path, chosen_activities_tree):
     try:
         num_duplicates = int(num_duplicates_entry.get())
     except ValueError:
@@ -189,7 +189,7 @@ def duplicate_file(duplicate_window, num_duplicates_entry, base_excel_path, chos
 
 # Create the main window
 root = tk.Tk()
-root.title("Flow Manager")
+root.title("activity Manager")
 style = ttk.Style()
 style.theme_use('clam')
 
@@ -197,37 +197,33 @@ style.theme_use('clam')
 navbar_frame = tk.Frame(root)
 navbar_frame.pack(padx=12, pady=2, fill=tk.X)
 
-url_name_label = ttk.Label(navbar_frame, text="Enter URL")
-url_name_label.pack(side=tk.LEFT, padx=(0, 10))
-url_name_entry = ttk.Entry(navbar_frame)
-url_name_entry.pack(side=tk.LEFT, padx=(0, 10))
 
-add_flow_button = tk.Button(navbar_frame, text="Run", command=add_flow)
-add_flow_button.pack(side=tk.LEFT)
+add_activity_button = tk.Button(navbar_frame, text="Confirm Testcase", command=add_activity)
+add_activity_button.pack(side=tk.LEFT)
 
-create_Flow_button = tk.Button(navbar_frame, text="Create Flow", command=create_flow)
-create_Flow_button.pack(side=tk.RIGHT)
+create_activity_button = tk.Button(navbar_frame, text="Create activity", command=create_activity)
+create_activity_button.pack(side=tk.RIGHT)
 navbar_separator = ttk.Separator(root, orient='horizontal')
 navbar_separator.pack(fill=tk.X, pady=1)
 
-# Create frame for Flow name and description
-flow_frame = ttk.Frame(root)
-flow_frame.pack(padx=12, pady=5, fill=tk.X)
+# Create frame for activity name and description
+activity_frame = ttk.Frame(root)
+activity_frame.pack(padx=12, pady=5, fill=tk.X)
 
 
 # Create frame for file selection
 file_frame = ttk.Frame(root)
 file_frame.pack(padx=12, pady=5, fill=tk.X)
 
-flow_entry = ttk.Entry(file_frame)
-flow_entry.pack(side=tk.LEFT, padx=(0, 10))
-flow_button = ttk.Button(file_frame, text="Choose Folder", command=lambda: choose_folder(flow_entry))
-flow_button.pack(side=tk.LEFT)
+activity_entry = ttk.Entry(file_frame)
+activity_entry.pack(side=tk.LEFT, padx=(0, 10))
+activity_button = ttk.Button(file_frame, text="Choose Folder", command=lambda: choose_folder(activity_entry))
+activity_button.pack(side=tk.LEFT)
 
-flow_button = ttk.Entry(file_frame)
-testcase_button = ttk.Button(file_frame, text="Choose File", command=lambda: choose_file(flow_button))
+activity_button = ttk.Entry(file_frame)
+testcase_button = ttk.Button(file_frame, text="Choose File", command=lambda: choose_file(activity_button))
 testcase_button.pack(side=tk.RIGHT)
-flow_button.pack(side=tk.RIGHT, padx=(20, 10))
+activity_button.pack(side=tk.RIGHT, padx=(20, 10))
 
 
 # Create frame for Listboxes and their labels
@@ -235,44 +231,44 @@ listbox_frame = ttk.Frame(root)
 listbox_frame.pack(padx=12, pady=5, fill=tk.BOTH, expand=True)
 
 # Frame for Available Activities
-available_flows_frame = ttk.Frame(listbox_frame)
-available_flows_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=(0, 5))
+available_activities_frame = ttk.Frame(listbox_frame)
+available_activities_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=(0, 5))
 
-available_flows_label = ttk.Label(available_flows_frame, text="Available Flows")
-available_flows_label.pack(side=tk.TOP, fill=tk.X)
+available_activities_label = ttk.Label(available_activities_frame, text="Available activities")
+available_activities_label.pack(side=tk.TOP, fill=tk.X)
 
-available_flows_tree = ttk.Treeview(available_flows_frame, columns=('Flow', 'Edit'), show='headings')
-available_flows_tree.heading('Flow', text='Flow')
-available_flows_tree.heading('Edit', text='Edit')
-available_flows_tree.column('Flow', width=200) 
-available_flows_tree.column('Edit', width=100, anchor='center')
+available_activities_tree = ttk.Treeview(available_activities_frame, columns=('activity', 'Edit'), show='headings')
+available_activities_tree.heading('activity', text='activity')
+available_activities_tree.heading('Edit', text='Edit')
+available_activities_tree.column('activity', width=200) 
+available_activities_tree.column('Edit', width=100, anchor='center')
 
-av_act_vscroll = ttk.Scrollbar(available_flows_frame, orient="vertical", command=available_flows_tree.yview)
-av_act_hscroll = ttk.Scrollbar(available_flows_frame, orient="horizontal", command=available_flows_tree.xview)
-available_flows_tree.configure(xscrollcommand=av_act_hscroll.set)
-av_act_hscroll.configure(command=available_flows_tree.xview)
+av_act_vscroll = ttk.Scrollbar(available_activities_frame, orient="vertical", command=available_activities_tree.yview)
+av_act_hscroll = ttk.Scrollbar(available_activities_frame, orient="horizontal", command=available_activities_tree.xview)
+available_activities_tree.configure(xscrollcommand=av_act_hscroll.set)
+av_act_hscroll.configure(command=available_activities_tree.xview)
 av_act_vscroll.pack(side=tk.RIGHT, fill=tk.Y)
 av_act_hscroll.pack(side=tk.BOTTOM, fill=tk.X)
-available_flows_tree.pack(fill=tk.BOTH, expand=True)
+available_activities_tree.pack(fill=tk.BOTH, expand=True)
 
 # Frame for Chosen Activities
-chosen_flows_frame = ttk.Frame(listbox_frame)
-chosen_flows_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=(5, 0))
+chosen_activities_frame = ttk.Frame(listbox_frame)
+chosen_activities_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=(5, 0))
 
-chosen_flows_label = ttk.Label(chosen_flows_frame, text="Chosen Flows")
-chosen_flows_label.pack(side=tk.TOP, fill=tk.X)
+chosen_activities_label = ttk.Label(chosen_activities_frame, text="Chosen activities")
+chosen_activities_label.pack(side=tk.TOP, fill=tk.X)
 
-chosen_flows_tree = ttk.Treeview(chosen_flows_frame, columns=('Flow'), show='headings')
-chosen_flows_tree.heading('Flow', text='Flow')
-chosen_flows_tree.column('Flow', width=300, stretch=tk.YES)
+chosen_activities_tree = ttk.Treeview(chosen_activities_frame, columns=('activity'), show='headings')
+chosen_activities_tree.heading('activity', text='activity')
+chosen_activities_tree.column('activity', width=300, stretch=tk.YES)
 
-ch_act_vscroll = ttk.Scrollbar(chosen_flows_frame, orient="vertical", command=chosen_flows_tree.yview)
-ch_act_hscroll = ttk.Scrollbar(chosen_flows_frame, orient="horizontal", command=chosen_flows_tree.xview)
-chosen_flows_tree.configure(xscrollcommand=ch_act_hscroll.set)
-ch_act_hscroll.configure(command=chosen_flows_tree.xview)
+ch_act_vscroll = ttk.Scrollbar(chosen_activities_frame, orient="vertical", command=chosen_activities_tree.yview)
+ch_act_hscroll = ttk.Scrollbar(chosen_activities_frame, orient="horizontal", command=chosen_activities_tree.xview)
+chosen_activities_tree.configure(xscrollcommand=ch_act_hscroll.set)
+ch_act_hscroll.configure(command=chosen_activities_tree.xview)
 ch_act_vscroll.pack(side=tk.RIGHT, fill=tk.Y)
 ch_act_hscroll.pack(side=tk.BOTTOM, fill=tk.X)
-chosen_flows_tree.pack(fill=tk.BOTH, expand=True)
+chosen_activities_tree.pack(fill=tk.BOTH, expand=True)
 
 # Change cursor on hover over 'Edit'
 def change_cursor(event):
@@ -281,17 +277,17 @@ def change_cursor(event):
     else:
         event.widget.config(cursor="")
 
-available_flows_tree.bind('<Motion>', change_cursor)
-chosen_flows_tree.bind('<Motion>', change_cursor)
+available_activities_tree.bind('<Motion>', change_cursor)
+chosen_activities_tree.bind('<Motion>', change_cursor)
 
 # Binding events
-available_flows_tree.bind('<Double-1>', add_to_chosen_flows)
-chosen_flows_tree.bind('<Double-1>', delete_from_chosen_flows)
-available_flows_tree.bind('<Motion>', lambda e: show_tooltip(e, available_flows_tree))
-chosen_flows_tree.bind('<Motion>', lambda e: show_tooltip(e, chosen_flows_tree))
+available_activities_tree.bind('<Double-1>', add_to_chosen_activities)
+chosen_activities_tree.bind('<Double-1>', delete_from_chosen_activities)
+available_activities_tree.bind('<Motion>', lambda e: show_tooltip(e, available_activities_tree))
+chosen_activities_tree.bind('<Motion>', lambda e: show_tooltip(e, chosen_activities_tree))
 
-available_flows_tree.bind('<Button-1>', on_treeview_click)
-chosen_flows_tree.bind('<Button-1>', on_treeview_click)
+available_activities_tree.bind('<Button-1>', on_treeview_click)
+chosen_activities_tree.bind('<Button-1>', on_treeview_click)
 
 # Create frame for Excel generation and folder selection
 excel_frame = ttk.Frame(root)
